2ae89ce0e60dbe9a03ca870c7f034ea3b99e4beb
star-eyes <66997443+star-eyes@users.noreply.github.com>
2020-12-20 17:35:23 +0800

    Revert "成绩管理系统（最终版）"
    This reverts commit d96f71d4d7f538df7bb961976ddce845482a5b6f.

--- 成绩管理系统最终版（以这个为准)/serv/main_views.py
+++
@@ -1,8 +0,0 @@
-from aiohttp import web
-from .config import db_block, web_routes, render_html
-
-
-@web_routes.get("/")
-async def home_page(request):
-    return web.HTTPFound(location="/student")
-
